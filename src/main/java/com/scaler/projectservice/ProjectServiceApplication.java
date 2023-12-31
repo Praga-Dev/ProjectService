a4f18cebf2a52151cc649b8db31ad47765e0b7b1
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2023-12-31 13:23:58 +0530

    HttpClient dependency updated

--- src/main/java/com/scaler/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/scaler/projectservice/ProjectServiceApplication.java
@@ -2,12 +2,21 @@ package com.scaler.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProjectServiceApplication.class, args);
+
     }
 
+    @Bean
+    public RestTemplateBuilder getRestTemplate() {
+        return new RestTemplateBuilder();
+    }
 }
